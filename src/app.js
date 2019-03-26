2c11aa6062a06229c6f111112b892b0f2475e673
Vit Krajicek <vit@krajicek.org>
2019-03-26 20:39:03 +0100

    CLSNO frontend separated, first commit

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+// import { PulseLoader } from 'react-spinners';
+
+
+import AppRouter from './routers/AppRouter';
+
+import {startLoggedUser }from './actions/userActions';
+import {startLoadTests} from './actions/testActions';
+
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+import 'babel-polyfill';
+
+const store = configureStore();
+
+
+document.documentElement.setAttribute('data-browser', navigator.userAgent);
+
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+// ReactDOM.render(<p>LOADING</p>, document.getElementById('app'))
+
+
+//startLogedUser and startLoadTests have custom callbacks -> after successfull    
+
+store.dispatch(startLoggedUser(() => {
+    // store.dispatch(startLoadTests(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+}));
+// }));
+
+
+
+
+
+
+//  ReactDOM.render(jsx, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  //extrahovat vsechna zkompilovana scss a css z bundle.js do styles.css
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+  
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                //i v development pouzivat sourceMap aby se v konzoli dalo dohledat, odkud styl pochazi
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            },
+            { 
+                test: /\.(png|jpg)$/, 
+                loader: 'url-loader?limit=8192'
+            },
+            { test: /\.json$/, loader: 'json-loader' }
+            ]
+        },
+        plugins: [CSSExtract],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            proxy: {'/api/**': {target: "http://127.0.0.1:3000"}},
+            historyApiFallback: true
+        }
+    }
+  };
